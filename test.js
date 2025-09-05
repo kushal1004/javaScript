430b6d92a53ac72fc6b3edd227215f339c989275
Kushal Singha <kushalsingha00@gmail.com>
2025-09-05 11:11:23 +0530

    practice of let var const

--- test.js
+++ test.js
@@ -1,2 +1,6 @@
-console.log("kushal singha")
-console.log("hello world")
+const employeeName = "kushal"
+const employeeAge  = "20"
+const employeeEmial = "kushal@gmail.com"
+employeePhone = "1234567890" // not preferable
+
+console.table([employeeName, employeeAge, employeeEmial, employeePhone])
