2ce88462a67ea61841464ec8f38cc3ccb0d38e92
Kushal Singha <kushalsingha00@gmail.com>
2025-09-05 11:31:48 +0530

    variable practices

--- test.js
+++
@@ -1,6 +0,0 @@
-const employeeName = "kushal"
-const employeeAge  = "20"
-const employeeEmial = "kushal@gmail.com"
-employeePhone = "1234567890" // not preferable
-
-console.table([employeeName, employeeAge, employeeEmial, employeePhone])
---
+++ variable.js
@@ -0,0 +1,38 @@
+const employeeName = "kushal"
+const employeeAge  = "20"
+
+// const employeeAge  = "21" // not allowed
+
+const employeeEmial = "kushal@gmail.com"
+
+ /* 
+ const cannot be updated
+  const cannot be redeclared
+  const is block scoped, it is not function scoped, it is not global scoped
+  */
+
+console.log(employeePhone) // undefined
+console.log(employeeSalary) // reference error for let and const also, it is called temoral dead zone
+
+var employeePhone = "9876543210"
+
+ /*
+ don't use var it is outdated, it can be updated, it can be redeclared, it can create bugs
+it is function scoped,it is not block scoped , it is global scoped
+ */
+
+employeePhone = "1234567890" // not preferable
+
+let employeeSalary = "50000"
+employeeSalary = "60000" 
+
+/*
+ preferable let can be updated
+ it is block scoped, it is not function scoped, it is not global scoped
+ value can be changed
+ but cannot be redeclared
+ */
+
+let employee; // declaration so output will be undefined
+
+console.table([employeeName, employeeAge, employeeEmial,employeePhone,employeeSalary,employee])
